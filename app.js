e48da244dc646f1629a1208fe714d9e21384a4a5
Artem9021 <68990883+Artem9021@users.noreply.github.com>
2020-10-03 07:50:29 +0300

    Security system update

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 
 
+
 const firebaseConfig = {
-	apiKey: "AIzaSyAHKgDIS_phtBzaZg3u2VZqHuTN_QmMfJI",
-	authDomain: "fir-chat-e85ad.firebaseapp.com",
-	databaseURL: "https://fir-chat-e85ad.firebaseio.com",
-	projectId: "fir-chat-e85ad",
-	storageBucket: "fir-chat-e85ad.appspot.com",
-	messagingSenderId: "266794827210",
-	appId: "1:266794827210:web:125446662fa0b2c77be333",
-	measurementId: "G-6GRRBM1H39"
+    apiKey: "AIzaSyAHKgDIS_phtBzaZg3u2VZqHuTN_QmMfJI",
+    authDomain: "fir-chat-e85ad.firebaseapp.com",
+    databaseURL: "https://fir-chat-e85ad.firebaseio.com",
+    projectId: "fir-chat-e85ad",
+    storageBucket: "fir-chat-e85ad.appspot.com",
+    messagingSenderId: "266794827210",
+    appId: "1:266794827210:web:125446662fa0b2c77be333",
+    measurementId: "G-6GRRBM1H39"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -17,37 +18,82 @@ firebase.initializeApp(firebaseConfig);
 name = "User"
 
 function sendMessage () {
-	// function to send a message into the database
-	let message = document.querySelector(".message").value;
-	message = message.replace("<", "[Invalid Character]");
-	message = message.replace(">", "[Invalid Character]");
+    // function to send a message into the database
+    let message = document.querySelector(".message").value;
+    while(message.indexOf("<")!=-1){
+        message = message.replace("<", "[Invalid Character]");
+    }
+    while(message.indexOf(">")!=-1){
+        message = message.replace(">", "[Invalid Character]");
+    }
+    while(message.indexOf("/")!=-1){
+        message = message.replace("/", "[Invalid Character]");
+    }
+    while(message.indexOf("eval")!=-1){
+        message = message.replace("eval", "[Invalid Expression]");
+    }
+    while(message.indexOf("img")!=-1){
+        message = message.replace("img", "[Invalid Expression]");
+    }
+    
 
-	
-		firebase.database().ref("messages").push().set({
-			"sender":name,
-			"message":message
 
-		})
-	document.querySelector(".message").value = "";	
+    
+        firebase.database().ref("messages").push().set({
+            "sender":name,
+            "message":message
 
-	return false;
+        })
+    document.querySelector(".message").value = "";    
+
+    return false;
 }
 
 function login () {
-	//console.log("function")
-	name = document.querySelector(".login-form").value;
-	name = name.replace("<", "[Invalid Character]");
-	name = name.replace(">", "[Invalid Character]");
-	if (name.length > 20){
-		name = name.slice(0, 20)
-	}
-	document.querySelector(".username").textContent = name;
-	document.querySelector(".login").classList.add("slide-up");
-	return false; 
+    //console.log("function")
+    name = document.querySelector(".login-form").value;
+    
+    while(name.indexOf("<")!=-1){
+        name = name.replace("<", "[Invalid Character]");
+    }
+    while(name.indexOf(">")!=-1){
+        name = name.replace(">", "[Invalid Character]");
+    }
+    while(name.indexOf("/")!=-1){
+        name = name.replace("/", "[Invalid Character]");
+    }
+    while(name.indexOf("eval")!=-1){
+        name = name.replace("eval", "[Invalid Expression]");
+    }
+    while(name.indexOf("img")!=-1){
+        name = name.replace("img", "[Invalid Expression]");
+    }
+
+    
+    if (name.length > 20){
+        name = name.slice(0, 20)
+    }
+    document.querySelector(".username").textContent = name;
+    document.querySelector(".login").classList.add("slide-up");
+    return false; 
 }
 
 firebase.database().ref("messages").on("child_added", (snapshot) => {
-	let html = document.querySelector(".messages").innerHTML;
-	document.querySelector(".messages").innerHTML = `<li><i class="fa fa-user-circle"></i><span class="message-text">${snapshot.val().sender}: ${snapshot.val().message} </span></li>` + html;
+    let html = document.querySelector(".messages").innerHTML;
+    
+    
+    
+    while(html.indexOf("eval")!=-1){
+        html = html.replace("eval", "[Invalid Expression]");
+    }
+    while(html.indexOf("img")!=-1){
+        html = html.replace("img", "[Invalid Expression]");
+    }
+    
+
+    
+    
+    
+    document.querySelector(".messages").innerHTML = `<li><i class="fa fa-user-circle"></i><span class="message-text">${snapshot.val().sender}: ${snapshot.val().message} </span></li>` + html;
 })
 
